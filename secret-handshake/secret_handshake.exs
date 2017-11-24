96f80d312c116a799fbac79e23fdd23fadc18b4a
Roman Pushkov <pushkovroman@me.com>
2017-11-25 02:34:24 +0300

    completed secret-handshake

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -1,4 +1,5 @@
 defmodule SecretHandshake do
+  @codes ["wink", "double blink", "close your eyes", "jump"]
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,7 +16,22 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    
+    cond do
+      code < 1 || code > 31 -> []
+      code > 15 ->
+        Integer.digits(code, 2)
+        |> Enum.slice(1, 5)
+        |> Enum.zip(Enum.reverse(@codes))
+        |> Enum.filter(fn({status, _code}) -> status == 1 end)
+        |> Enum.map(fn({1, code}) -> code end)
+      code <= 15 ->
+        Integer.digits(code, 2)
+        |> Enum.reverse
+        |> Enum.zip(@codes)
+        |> Enum.filter(fn({status, _code}) -> status == 1 end)
+        |> Enum.map(fn({1, code}) -> code end)
+    end
+    # Integer.digits(code, 2)
   end
 end
 
