752bf59c1cde014854c99c73e7ef93ac46fa9caa
Roman Pushkov <pushkovroman@me.com>
2017-11-28 17:04:20 +0300

    completed rotational-cipher

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -1,4 +1,5 @@
 defmodule RotationalCipher do
+  @int_values %{ "A" => 65, "Z" => 90, "a" => 97, "z" => 122 }
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -6,8 +7,29 @@ defmodule RotationalCipher do
   iex> RotationalCipher.rotate("Attack at dawn", 13)
   "Nggnpx ng qnja"
   """
+
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    shift = shift
+    |> abs
+    |> rem(26)
+
+    text
+    |> String.split("", trim: true)
+    |> Enum.map(fn x -> shift_char(x, shift, String.match?(x, ~r/[a-zA-Z]/)) end)
+    |> Enum.join("")
   end
+
+  defp shift_char(char, _shift, _is_a_letter = false), do: char
+  
+  defp shift_char(<<aacute::utf8>>, shift, _is_a_letter) do
+    cond do
+      aacute in @int_values["A"]..@int_values["Z"] -> 
+        <<(@int_values["A"] + rem(aacute - @int_values["A"] + shift, 26))::utf8>>
+      aacute in @int_values["a"]..@int_values["z"] -> 
+        <<(@int_values["a"] + rem(aacute - @int_values["a"] + shift, 26))::utf8>>
+    end
+  end
+
 end
 
